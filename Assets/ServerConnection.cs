8f98bc8abf6a127cda82a1a9e99bd25fd8e2ccf0
Kevin <kevin.abydos@gmail.com>
2015-02-17 10:37:36 +0000

    Fisrt Commit

---
+++ Assets/ServerConnection.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using NextGenServer;
+using UnityEngine.UI;
+
+public class ServerConnection : MonoBehaviour {
+
+	Connection connection = new Connection();
+	public Text useraname;
+	public Text email;
+	public Text password;
+	// Use this for initialization
+	void Start () 
+	{
+		connection.Connect ("192.168.1.12", 2000);
+		ServerEvents.AddEvent ("CustomEvent");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnApplicationQuit()
+	{
+		connection.CloseConnection ();
+	}
+
+	public void SignUp()
+	{
+		AccountDataBase.CreateAccount (useraname.text, email.text, password.text);
+	}
+
+	public void Login()
+	{
+		AccountDataBase.ValidateAccount (useraname.text, password.text);
+	}
+
+	public void CreateAvatar()
+	{
+		AvatarDatabase.AddAvatar ();
+	}
+
+	public void SaveAvatar()
+	{
+		AvatarDatabase.SaveAvatar ();
+	}
+}
